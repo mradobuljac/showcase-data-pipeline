a0f600510504fd6782fccfde96f6dc60dac596a3
DESKTOP-7EQLQLN\Marin <marin.radobuljac@gmail.com>
2024-01-19 10:13:00 +0100

    cleanup of showcase-data-pipeline dag

--- airflow/dags/showcase_data_pipeline.py
+++ airflow/dags/showcase_data_pipeline.py
@@ -7,15 +7,18 @@ from airflow.providers.amazon.aws.operators.lambda_function import (
 from airflow.providers.amazon.aws.operators.redshift_data import RedshiftDataOperator
 
 
-# max_active_runs = only one dagrun can be executing at any point in time
-# catchup = run all dagruns from start_date till current data
 @dag(
-    start_date=datetime(2024, 1, 1), max_active_runs=1, schedule="@daily", catchup=False
+    start_date=datetime(2024, 1, 1),
+    schedule="@daily",  # at midnight
+    default_args={"depends_on_past": True},  # all tasks will have this option enabled
+    max_active_runs=1,  # only one DAG run can be executing at any point in time
+    catchup=False,  # run all DAG runs from start_date till current data
 )
 def showcase_data_pipeline():
-    # logical date that governs whole data pipeline
+    # {{ ds }} is a template for logical date. On runtime will be resolved into yyyy-mm-dd corresponding to each dagrun
     payload = {"date": "{{ ds }}"}
 
+    # creating and initializing Redshift objects
     redshift_ddl_setup = RedshiftDataOperator(
         task_id="redshift_DDL_setup",
         aws_conn_id="aws_conn",
@@ -24,27 +27,27 @@ def showcase_data_pipeline():
         db_user="awsuser",
         sql="./sql/DDL_setup.sql",
         wait_for_completion=True,
-        depends_on_past=True,
     )
 
+    # get products data from API endpoint and upload to S3 bucket
     products_from_api_to_s3 = LambdaInvokeFunctionOperator(
         task_id="products_to_s3",
         aws_conn_id="aws_conn",
         function_name="showcase_data_pipeline_products_to_s3",
         invocation_type="RequestResponse",
         payload=json.dumps(payload),
-        depends_on_past=True,
     )
 
+    # get sales data from API endpoint and upload to S3 bucket
     sales_from_api_to_s3 = LambdaInvokeFunctionOperator(
         task_id="sales_to_s3",
         aws_conn_id="aws_conn",
         function_name="showcase_data_pipeline_sales_to_s3",
         invocation_type="RequestResponse",
         payload=json.dumps(payload),
-        depends_on_past=True,
     )
 
+    # COPY products data from S3 bucket into Redshift staging table
     s3_to_redshift_stage_products = RedshiftDataOperator(
         task_id="s3_to_redshift_stage_products",
         aws_conn_id="aws_conn",
@@ -53,9 +56,9 @@ def showcase_data_pipeline():
         db_user="awsuser",
         sql="./sql/s3_to_stage_products.sql",
         wait_for_completion=True,
-        depends_on_past=True,
     )
 
+    # COPY sales data from S3 bucket into Redshift staging table
     s3_to_redshift_stage_sales = RedshiftDataOperator(
         task_id="s3_to_redshift_stage_sales",
         aws_conn_id="aws_conn",
@@ -64,9 +67,9 @@ def showcase_data_pipeline():
         db_user="awsuser",
         sql="./sql/s3_to_stage_sales.sql",
         wait_for_completion=True,
-        depends_on_past=True,
     )
 
+    # upsert dim_products table with new data
     upsert_dim_products = RedshiftDataOperator(
         task_id="upsert_dim_products",
         aws_conn_id="aws_conn",
@@ -75,9 +78,9 @@ def showcase_data_pipeline():
         db_user="awsuser",
         sql="./sql/upsert_dim_products.sql",
         wait_for_completion=True,
-        depends_on_past=True,
     )
 
+    # upsert fact_sales table with new data
     upsert_fact_sales = RedshiftDataOperator(
         task_id="upsert_fact_sales",
         aws_conn_id="aws_conn",
@@ -86,9 +89,9 @@ def showcase_data_pipeline():
         db_user="awsuser",
         sql="./sql/upsert_fact_sales.sql",
         wait_for_completion=True,
-        depends_on_past=True,
     )
 
+    # dependencies setup
     redshift_ddl_setup >> [products_from_api_to_s3, sales_from_api_to_s3]
     products_from_api_to_s3 >> s3_to_redshift_stage_products
     sales_from_api_to_s3 >> s3_to_redshift_stage_sales
